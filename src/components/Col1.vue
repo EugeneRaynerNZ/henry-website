f910f02fe9f573d83ae3e479b0974bf96f13314d
hdemal <hdemalmanche@gmail.com>
2022-04-30 18:04:26 +1200

    Update Col1 colourblind
    added colourblind

--- src/components/Col1.vue
+++ src/components/Col1.vue
@@ -8,6 +8,7 @@
 import Vue from 'vue'
 import Artwork from './Artwork.vue'
 import AbstractCloningVector from "../assets/image1.jpg"
+import ColourBlind from "../assets/colourblind.jpg"
 // import Image2 from "../assets/2.jpg"
 // import Image3 from "../assets/3.jpg"
 // import Image4 from "../assets/4.jpg"
@@ -28,10 +29,10 @@ export default Vue.extend({
                 fullDescription: ['paragraph 1', 'paragraph 2', 'paragraph 3']
               },
               {
-                id: 'the-blue-atom-2', 
-                title: 'The Blue Atom 2',
-                src: Image,
-                summary: 'A one of a kind artwork produced by myself which showcases the way in which atoms in the universe coincide with waves in the ocean.',
+                id: 'colour-blind', 
+                title: 'Colourblind',
+                src: ColourBlind,
+                summary: 'Acrylic on canvas, 2020, 40 x 40cm',
                 fullDescription: ['paragraph 1', 'paragraph 2', 'paragraph 3']
               },
               {
